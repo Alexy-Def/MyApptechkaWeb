6933fdaadd10d0d1f98da7f3cb2dccc189577ad9
JOKER <sasha.golubovich@tut.by>
2021-06-16 20:05:21 +0300

    Add BaseRepository and IBase Repository

---
+++ MyApptechkaWeb/MyApptechkaWeb/EfStuff/Repositories/BaseRepository.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpaceWeb.EfStuff.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaceWeb.EfStuff.Repositories
+{
+    public abstract class BaseRepository<ModelType> 
+        : IBaseRepository<ModelType> where ModelType : BaseModel
+    {
+        protected SpaceDbContext _spaceDbContext;
+        protected DbSet<ModelType> _dbSet;
+
+        public BaseRepository(SpaceDbContext spaceDbContext)
+        {
+            _spaceDbContext = spaceDbContext;
+            _dbSet = _spaceDbContext.Set<ModelType>();
+        }
+
+        public virtual List<ModelType> GetAll()
+        {
+            return _dbSet.ToList();
+        }
+
+        public virtual ModelType Get(long id)
+        {
+            return _dbSet.SingleOrDefault(x => x.Id == id);
+        }
+
+        public virtual void Save(ModelType model)
+        {
+            if (model.Id > 0)
+            {
+                _dbSet.Update(model);
+            }
+            else
+            {
+                _dbSet.Add(model);
+            }
+            _spaceDbContext.SaveChanges();
+        }
+
+        public virtual void Remove(long id)
+        {
+            var model = Get(id);
+            Remove(model);
+        }
+
+        public virtual void Remove(ModelType model)
+        {
+            _spaceDbContext.Remove(model);
+            _spaceDbContext.SaveChanges();
+        }
+
+        public virtual void Remove(IEnumerable<long> ids)
+        {
+            foreach (var userid in ids)
+            {
+                Remove(userid);
+            }
+        }
+    }
+}
