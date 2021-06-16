86b801b410ab320e08384fc1217e0bcbe231b065
JOKER <sasha.golubovich@tut.by>
2021-06-16 11:59:36 +0300

    Edited layout.css

--- MyApptechkaWeb/MyApptechkaWeb/Controllers/UserController.cs
+++ MyApptechkaWeb/MyApptechkaWeb/Controllers/UserController.cs
@@ -11,27 +11,11 @@ namespace MyApptechkaWeb.Controllers
 {
     public class UserController : Controller
     {
-        private readonly ILogger<UserController> _logger;
+        
 
-        public UserController(ILogger<UserController> logger)
+        public UserController()
         {
-            _logger = logger;
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
