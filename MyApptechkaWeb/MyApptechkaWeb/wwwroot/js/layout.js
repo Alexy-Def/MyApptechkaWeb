40eb44487657dcd2f93b381a5978c86747b9afd1
JOKER <sasha.golubovich@tut.by>
2021-06-26 23:55:08 +0300

    Edit popup and layout

--- MyApptechkaWeb/MyApptechkaWeb/wwwroot/js/layout.js
+++ MyApptechkaWeb/MyApptechkaWeb/wwwroot/js/layout.js
@@ -6,4 +6,26 @@ $(document).ready(function () {
 	$('.log-in').click(function () {
 		$('.nice-popup').removeClass('hide');
 	});
+
+	$('.switcher-login-btn').click(function () {
+		$('.switcher-login-btn').addClass('selected-reg-login');
+		$('.switcher-login-btn').addClass('selected-login');
+		$('.switcher-reg-btn').removeClass('selected-reg-login');
+		$('.switcher-reg-btn').removeClass('selected-reg');
+		$('.confirmPassword').addClass('hide');
+		$('.phoneNumber').addClass('hide');
+		$('.login').removeClass('hide');
+		$('.reg').addClass('hide');
+	});
+
+	$('.switcher-reg-btn').click(function () {
+		$('.switcher-reg-btn').addClass('selected-reg-login');
+		$('.switcher-reg-btn').addClass('selected-reg');
+		$('.switcher-login-btn').removeClass('selected-reg-login');
+		$('.switcher-login-btn').removeClass('selected-login');
+		$('.confirmPassword').removeClass('hide');
+		$('.phoneNumber').removeClass('hide');
+		$('.login').addClass('hide');
+		$('.reg').removeClass('hide');
+	});
 });
