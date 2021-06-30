828f407eb96aef2cc8805beb5179087796c1deb7
JOKER <sasha.golubovich@tut.by>
2021-06-30 17:22:59 +0300

    Edit code-from-sms

--- MyApptechkaWeb/MyApptechkaWeb/wwwroot/js/layout.js
+++ MyApptechkaWeb/MyApptechkaWeb/wwwroot/js/layout.js
@@ -1,5 +1,38 @@
 $(document).ready(function () {
-	$("#test").focus();
+	var arr = ['.test1', '.test2', '.test3', '.test4'];
+	//var t = arr[0];
+	//var tt = `${arr[0]}`;
+	$('.test999').click(function () {
+		$('.test1').focus();
+		for (var i = 0; i < 4; i++) {
+			var selector = `${arr[i]}`;
+			var selectorNext = `${arr[i+1]}`;
+			$(selector).keyup(function () {
+				$(selector).next(selectorNext).focus();
+			})
+		}
+		//console.log(t);
+		//console.log(tt);
+
+		//$('.test1').keyup(function () {
+		//	$(this).next('.test2').focus();
+		//})
+		//$('.test2').keyup(function () {
+		//	$(this).next('.test3').focus();
+		//})
+		//$('.test3').keyup(function () {
+		//	$(this).next('.test4').focus();
+		//})
+    })
+	//for (var i = 0; i < 4; i++) {
+	//	$(`\'${arr[i]}\'`).keyup(function () {
+	//		$(this).next(`\'${arr[i + 1]}\'`).focus();
+ //       })
+ //   }
+
+	//$('.test1').keyup(function () {
+	//	$(this).next('.test2').focus();
+ //   })
 
 	$('.popup-cover').click(function () {
 		$('.nice-popup').addClass('hide');
