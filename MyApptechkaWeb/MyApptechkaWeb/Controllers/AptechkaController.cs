3cf3d3e37fc6b56e8b5e9585e2e4b21535e9b521
JOKER <sasha.golubovich@tut.by>
2021-07-11 21:12:13 +0300

    Add AptechkaController and .cshtml

---
+++ MyApptechkaWeb/MyApptechkaWeb/Controllers/AptechkaController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MyApptechkaWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyApptechkaWeb.Controllers
+{
+    public class AptechkaController : Controller
+    {
+        
+
+        public AptechkaController()
+        {
+            
+        }
+
+        [HttpGet]
+        public IActionResult Aptechka()
+        {
+            return View();
+        }
+
+    }
+}
