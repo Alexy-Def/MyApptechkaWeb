5b11e06b3d04c0b878013e89a3d23c094ddf207b
JOKER <sasha.golubovich@tut.by>
2021-07-14 12:51:33 +0300

    Add AddPtechkaRepository (+interface)

--- MyApptechkaWeb/MyApptechkaWeb/Controllers/AptechkaController.cs
+++ MyApptechkaWeb/MyApptechkaWeb/Controllers/AptechkaController.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MyApptechkaWeb.EfStuff.Repositories.IRepository;
 using MyApptechkaWeb.Models;
+using MyApptechkaWeb.Service.Interface;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,11 +14,15 @@ namespace MyApptechkaWeb.Controllers
 {
     public class AptechkaController : Controller
     {
-        
+        private IUserService _userService;
+        private IAptechkaRepository _aptechkaRepository;
+        private IPathHelperService _pathHelperService;
 
-        public AptechkaController()
+        public AptechkaController(IUserService userService, IAptechkaRepository aptechkaRepository, IPathHelperService pathHelperService)
         {
-            
+            _userService = userService;
+            _aptechkaRepository = aptechkaRepository;
+            _pathHelperService = pathHelperService;
         }
 
         [HttpGet]
@@ -24,5 +31,25 @@ namespace MyApptechkaWeb.Controllers
             return View();
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Aptechka(AddAptechkaViewModel viewModel)
+        {
+            var user = _userService.GetCurrent();
+             //var newAptechka = 
+
+            if (viewModel.AptechkaPicture != null)
+            {
+                var pathTo = _pathHelperService.GetPathToAvatarByUser(user.Id);
+                using (var fileStream = new FileStream(pathTo, FileMode.OpenOrCreate))
+                {
+                    await viewModel.AptechkaPicture.CopyToAsync(fileStream);
+                }
+                //user.AvatarUrl = _pathHelperService.GetAvatarUrlByUser(user.Id);
+
+                //_userRepository.Save(user);
+            }
+
+            return View();
+        }
     }
 }
--- MyApptechkaWeb/MyApptechkaWeb/Models/AddAptechkaViewModel.cs
+++ MyApptechkaWeb/MyApptechkaWeb/Models/AddAptechkaViewModel.cs
@@ -1,12 +1,17 @@
+using Microsoft.AspNetCore.Http;
 using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace MyApptechkaWeb.Models
 {
-    public class ProfileViewModel
+    public class AddAptechkaViewModel
     {
-        public string Login { get; set; }
-        public string Password { get; set; }
-        public string AvatarUrl { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Location { get; set; }
+        public string Color { get; set; }
+        public string AdditionalDescription { get; set; }
+        public IFormFile AptechkaPicture { get; set; }
     }
 }
