f7b8eaf8eca3305f1c41fb4c62128d9d6b0b8424
JOKER <sasha.golubovich@tut.by>
2021-08-10 21:40:57 +0300

    Add AptechkaService

---
+++ MyApptechkaWeb/MyApptechkaWeb/Service/AptechkaService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using MyApptechkaWeb.EfStuff.Model;
+using MyApptechkaWeb.EfStuff.Repositories.IRepository;
+using MyApptechkaWeb.Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace MyApptechkaWeb.Service
+{
+    public class AptechkaService : IAptechkaService
+    {
+        private IUserService _userService;
+        public AptechkaService(IUserService userService)
+        {
+            _userService = userService;
+        }
+        
+        public string GetAptechkaPictureUrl()
+        {
+            var userAvatar = _userService.GetCurrent()?.AvatarUrl;
+            return !string.IsNullOrWhiteSpace(userAvatar)
+                ? userAvatar
+                : "/image/avatar/default-avatar.png";
+        }
+    }
+}
