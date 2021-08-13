6464c68082f7d5021decc1994affe9dc9b743a54
JOKER <sasha.golubovich@tut.by>
2021-08-13 18:59:21 +0300

    Add editting information about drugs

--- MyApptechkaWeb/MyApptechkaWeb/wwwroot/js/aptechka.js
+++ MyApptechkaWeb/MyApptechkaWeb/wwwroot/js/aptechka.js
@@ -14,4 +14,12 @@
     $('.aptechka-edit-info-popup-cover').click(function () {
         $('.aptechka-edit-info-popup').addClass('hide');
     })
+
+    $('.btn-action-drug.btn-edit-drug').click(function () {
+        $('.drug-edit-info-popup').removeClass('hide');
+    })
+
+    $('.drug-edit-info-popup-cover').click(function () {
+        $('.drug-edit-info-popup').addClass('hide');
+    })
 })
